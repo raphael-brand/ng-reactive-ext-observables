8aae7720af22744d6ee35fd18240b86281bf3083
Raphael Brand <info@raphaelbrand.de>
2016-12-19 20:25:13 +0100

    Initial commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,24 +11,6 @@ import {Observable} from 'rxjs/Rx';
 })
 export class AppComponent {
     constructor(){
-      var keyups = Observable.fromEvent($('#search'), 'keyup')
-        .map(e => e.target.value)
-        .filter(text => text.length >= 3)
-        .debounceTime(400)
-        .distinctUntilChanged()
-        .flatMap(searchTerm => {
-          var url = "https://api.spotify.com/v1/search?q="+searchTerm+"&type=artist";
-          var promise = $.getJSON(url);
-          return Observable.fromPromise(promise);
-        });
-
-      var subscription = keyups.subscribe(data => {
-        data.artists.items && data.artists.items.map(artist => {
-          console.log(artist.name);
-          // search only once
-          //subscription.unsubscribe();
-        })
-      });
-
+      
     }
 }
