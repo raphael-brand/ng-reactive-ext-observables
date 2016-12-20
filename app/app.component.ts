c34e7c2942c72b05ff4b071408bb110ef745fcdf
Raphael Brand <info@raphaelbrand.de>
2016-12-20 09:49:48 +0100

    excercise - create form

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,15 +2,22 @@ import {Component} from 'angular2/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/filter';
+import {ControlGroup, FormBuilder} from 'angular2/common';
 
 @Component({
     selector: 'my-app',
     template: `
-        <input id="search" type="text" class="form-control" placeholder="Search ...">
+        <form [ngFormModel]="form">
+          <input type="text" ngControl="search">
+        </form>
     `
 })
 export class AppComponent {
-    constructor(){
+    form: ControlGroup;
+    constructor(fb: FormBuilder){
+      this.form = fb.group({
+        search: []
+      });
       console.log(new Observable())
     }
 }
